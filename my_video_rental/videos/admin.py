4abab936be149da5d2ecb9db68235a0c26b7575f
jkcolourmoon <jithendra@thecolourmoon.com>
2018-12-27 18:54:17 +0530

    project compleate for admin advanced topics successfully

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['title','release_year','length']
+    search_fields = ['title','release_year']
+    list_filter = ['release_year','title','length']
+    list_display = ['title','release_year','length']
+    list_editable = ['length','release_year']
+
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ['first_name','last_name','phone']
+    list_display = ['first_name','last_name','phone']
+
+admin.site.register(models.Movie,MovieAdmin)
+admin.site.register(models.Customer,CustomerAdmin)
--- my_video_rental/videos/models.py
+++ my_video_rental/videos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title       = models.CharField(max_length=250)
+    length      = models.PositiveIntegerField()
+    release_year = models.PositiveIntegerField()
+    def __str__(self):
+        return self.title
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    phone     = models.PositiveIntegerField()
+    def __str__(self):
+        return self.first_name + ' '+ self.last_name
